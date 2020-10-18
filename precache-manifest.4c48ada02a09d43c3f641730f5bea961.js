ed1f924b0b23c882392d941835d3d38cfaee322a
Taman <twinp.st@gmail.com>
2020-10-18 18:23:31 +0100

    Updates

---
+++ precache-manifest.4c48ada02a09d43c3f641730f5bea961.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561ba3a11405ba6a9dc7ec889f1d694c",
+    "url": "/product-opportunity-assesment-form/index.html"
+  },
+  {
+    "revision": "4ab1708275fac3fc58d7",
+    "url": "/product-opportunity-assesment-form/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "024c1319867d9a5089ab",
+    "url": "/product-opportunity-assesment-form/static/js/2.c0129143.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/product-opportunity-assesment-form/static/js/2.c0129143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab1708275fac3fc58d7",
+    "url": "/product-opportunity-assesment-form/static/js/main.4da6a53e.chunk.js"
+  },
+  {
+    "revision": "25d0867bf3a0b64d06c3",
+    "url": "/product-opportunity-assesment-form/static/js/runtime-main.ef5092dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/product-opportunity-assesment-form/static/media/logo.5d5d9eef.svg"
+  }
+]);
